094324cd3a6f672e97cef6682d18d6df994759b3
Siddharth Pandiya <siddharthpandiya95@gmail.com>
2018-11-12 15:15:58 -0800

    cut down crime dataset

--- SafeT/ViewController.swift
+++ SafeT/ViewController.swift
@@ -9,9 +9,10 @@
 import UIKit
 import MapKit
 import CoreLocation
+import CSV
 
 class ViewController: UIViewController, CLLocationManagerDelegate {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -22,8 +23,15 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         locationManager.startUpdatingLocation()
         let locValue:CLLocationCoordinate2D = locationManager.location!.coordinate
         print("locations = \(locValue.latitude) \(locValue.longitude)")
+        
+        let stream = InputStream(fileAtPath: "/path/to/file.csv")!
+        let csv = try! CSVReader(stream: stream)
+        while let row = csv.next() {
+            print("\(row)")
+        }
     }
 
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
