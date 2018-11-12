20f9290d5caf71a4a9ba55967c1b5365968998d4
Siddharth Pandiya <siddharthpandiya95@gmail.com>
2018-11-12 14:16:49 -0800

    retrieve user location

--- SafeT/ViewController.swift
+++ SafeT/ViewController.swift
@@ -7,19 +7,33 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let locationManager = CLLocationManager()
+        locationManager.delegate = self;
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestAlwaysAuthorization()
+        locationManager.startUpdatingLocation()
+        let locValue:CLLocationCoordinate2D = locationManager.location!.coordinate
+        print("locations = \(locValue.latitude) \(locValue.longitude)")
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let locValue:CLLocationCoordinate2D = manager.location!.coordinate
+        print("locations = \(locValue.latitude) \(locValue.longitude)")
+    }
 
 }
 
